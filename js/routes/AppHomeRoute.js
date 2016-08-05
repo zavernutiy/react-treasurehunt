95c1ab9b9892722fae54e31c96625ec4c4cb077e
Max Zavernenkov <zavernutiy@Mac-mini-de-TMW.local>
2016-08-05 17:50:35 +0300

    Finished the tutorial

--- js/routes/AppHomeRoute.js
+++ js/routes/AppHomeRoute.js
@@ -1,12 +1,9 @@
 import Relay from 'react-relay';
 
 export default class extends Relay.Route {
-  static queries = {
-    viewer: () => Relay.QL`
-      query {
-        viewer
-      }
-    `,
-  };
-  static routeName = 'AppHomeRoute';
+    static path = '/';
+    static queries = {
+        game: () => Relay.QL`query { game }`,
+    };
+    static routeName = 'AppHomeRoute';
 }
